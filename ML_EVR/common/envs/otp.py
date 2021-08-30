0b86418ceadde317467a31a8659cbe0975e98252
Icould <aykut@mail.bg>
2021-08-30 22:18:58 +0300

    fix

--- ML_EVR/common/envs/otp.py
+++ ML_EVR/common/envs/otp.py
@@ -1,3 +1,4 @@
+import copy
 import random
 
 from ML_EVR.common.envs.base_env import ClassificationEnv
@@ -18,17 +19,41 @@ class EvoClsEnv(ClassificationEnv):
 class SortedScenariosClsEnvLPT(ClassificationEnv):
     def __init__(self, scenarios, executors):
         super(SortedScenariosClsEnvLPT, self).__init__(scenarios, executors)
-        self._scenarios_def.sort(reverse=True)
+        tmp = []
+        for i in range(self._scenarios_count):
+            tmp.append((self._scenarios_def[i], self._scenarios[i]))
+        tmp.sort(reverse=True, key=lambda pair: pair[0])
+        for i in range(self._scenarios_count):
+            self._scenarios_def[i] = tmp[i][0]
+            self._scenarios[i] = copy.deepcopy(tmp[i][1])
 
     def _special_action_on_reset(self):
-        self._scenarios_def.sort(reverse=True)
+        tmp = []
+        for i in range(self._scenarios_count):
+            tmp.append((self._scenarios_def[i], self._scenarios[i]))
+        tmp.sort(reverse=True, key=lambda pair: pair[0])
+        for i in range(self._scenarios_count):
+            self._scenarios_def[i] = tmp[i][0]
+            self._scenarios[i] = copy.deepcopy(tmp[i][1])
 
 
 class SortedScenariosClsEnvSPT(ClassificationEnv):
     def __init__(self, scenarios, executors):
         super(SortedScenariosClsEnvSPT, self).__init__(scenarios, executors)
-        self._scenarios_def.sort()
+        tmp = []
+        for i in range(self._scenarios_count):
+            tmp.append((self._scenarios_def[i], self._scenarios[i]))
+        tmp.sort(key=lambda pair: pair[0])
+        for i in range(self._scenarios_count):
+            self._scenarios_def[i] = tmp[i][0]
+            self._scenarios[i] = copy.deepcopy(tmp[i][1])
 
     def _special_action_on_reset(self):
-        self._scenarios_def.sort()
+        tmp = []
+        for i in range(self._scenarios_count):
+            tmp.append((self._scenarios_def[i], self._scenarios[i]))
+        tmp.sort(key=lambda pair: pair[0])
+        for i in range(self._scenarios_count):
+            self._scenarios_def[i] = tmp[i][0]
+            self._scenarios[i] = copy.deepcopy(tmp[i][1])
 
